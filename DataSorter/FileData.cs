a655ef3f7817c61bb2c918d0c8daa121a0b9cf4f
sheinv78 <48327620+sheinv78@users.noreply.github.com>
2021-08-18 13:52:35 +0300

    Light decomposition to improve readability and maintainability.

---
+++ DataSorter/FileData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace DataSorter
+{
+    internal class FileData : IComparable<FileData>
+    {
+        private FileData()
+        {
+            NumericPart = 0;
+            StringPart = string.Empty;
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+        public uint NumericPart { get; set; }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
+        public string StringPart { get; set; }
+
+        public static FileData FromString(string str)
+        {
+            var parts = str.Split(". ");
+
+            if (parts.Length != 2)
+            {
+                throw new InvalidOperationException($"The string {str} couldn't be parsed into FileData instance.");
+            }
+
+            return new FileData()
+            {
+                NumericPart = uint.Parse(parts[0]),
+                StringPart = parts[1]
+            };
+        }
+
+        public int CompareTo(FileData? other)
+        {
+            if (other == null)
+                return 1;
+
+            var stringCmpResult = string.Compare(StringPart, other.StringPart, StringComparison.Ordinal);
+
+            return stringCmpResult != 0 ? stringCmpResult : NumericPart.CompareTo(other.NumericPart);
+        }
+
+        public override string ToString()
+        {
+            return new StringBuilder().AppendJoin(". ", NumericPart, StringPart).ToString();
+        }
+    }
+}
--- DataSorter/Program.cs
+++ DataSorter/Program.cs
@@ -4,86 +4,14 @@ using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.IO;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using MoreComplexDataStructures;
 
 namespace DataSorter
 {
-    internal class FileData : IComparable<FileData>
-    {
-        private FileData()
-        {
-            NumericPart = 0;
-            StringPart = string.Empty;
-        }
-
-        // ReSharper disable once MemberCanBePrivate.Global
-        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
-        public uint NumericPart { get; set; }
-
-        // ReSharper disable once MemberCanBePrivate.Global
-        // ReSharper disable once PropertyCanBeMadeInitOnly.Global
-        public string StringPart { get; set; }
-
-        public static FileData FromString(string str)
-        {
-            var parts = str.Split(". ");
-
-            if (parts.Length != 2)
-            {
-                throw new InvalidOperationException($"The string {str} couldn't be parsed into FileData instance.");
-            }
-
-            return new FileData()
-            {
-                NumericPart = uint.Parse(parts[0]),
-                StringPart = parts[1]
-            };
-        }
-
-        public int CompareTo(FileData? other)
-        {
-            if (other == null)
-                return 1;
-
-            var stringCmpResult = string.Compare(StringPart, other.StringPart, StringComparison.Ordinal);
-
-            return stringCmpResult != 0 ? stringCmpResult : NumericPart.CompareTo(other.NumericPart);
-        }
-
-        public override string ToString()
-        {
-            return new StringBuilder().AppendJoin(". ", NumericPart, StringPart).ToString();
-        }
-   }
-
-    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
-    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
-    internal class FileDataReaderItem : IComparable<FileDataReaderItem>
-    {
-        public FileDataReaderItem(FileData fileData, StreamReader streamReader, string fileName)
-        {
-            FileData = fileData;
-            StreamReader = streamReader;
-            FileName = fileName;
-        }
-
-        public FileData FileData { get; set; }
-        public StreamReader StreamReader { get; set; }
-
-        public string FileName { get; set; }
-
-        public int CompareTo(FileDataReaderItem? other)
-        {
-            return FileData.CompareTo(other?.FileData);
-        }
-    }
-
     internal static class Program
     {
         private static async Task<int> Main(string[] args)
